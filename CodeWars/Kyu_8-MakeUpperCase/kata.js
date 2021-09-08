90321b1d779e8c42c2d760cc11298020d39b564a
Mohamed Hossam <m.hossameldine@gmail.com>
2021-09-08 14:45:05 +0200

    Kyu 8 - Make Uppercase

---
+++ CodeWars/Kyu_8-MakeUpperCase/kata.js
@@ -0,0 +1,10 @@
+/*
+Write a function which converts the input string to uppercase.
+*/
+
+function makeUpperCase(string) {
+  return string.toUpperCase();
+}
+
+console.log(makeUpperCase("alaa"));    // -> ALAAN
+console.log(makeUpperCase("teSt"));    // -> TEST
