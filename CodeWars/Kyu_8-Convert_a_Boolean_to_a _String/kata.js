a8500cd3c9182c6431a0f2a96f1fbc68209d2556
Mohamed Hossam <m.hossameldine@gmail.com>
2021-09-17 14:40:17 +0200

    8 Kyu - Conver a Boolean to a String

---
+++ CodeWars/Kyu_8-Convert_a_Boolean_to_a _String/kata.js
@@ -0,0 +1,16 @@
+/*
+Convert a Boolean to a String:
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+*/
+function booleanToString(b){
+  return String(b);
+}
+
+// Other Solution:
+/*
+return b.toString();
+return `${b};
+return b? 'true': 'false';
+*/
