d79261410fdce461a94e8d89e914bfb7e4bba021
Mohamed Hossam <m.hossameldine@gmail.com>
2021-09-26 01:58:53 +0200

    8 Kyu - Remove all exclamation marks from the end of sentence

---
+++ CodeWars/Kyu_8-Exclamation_Marks_Series_#2-Remove_All_Exclamation_Marks_From_The_End_Of_Sentence/kata.js
@@ -0,0 +1,42 @@
+/*
+Exclamation marks series #2: Remove all exclamation marks from the end of sentence:
+
+Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples
+remove("Hi!") === "Hi"
+remove("Hi!!!") === "Hi"
+remove("!Hi") === "!Hi"
+remove("!Hi!") === "!Hi"
+remove("Hi! Hi!") === "Hi! Hi"
+remove("Hi") === "Hi"
+
+*/
+
+function remove(s){
+  s = [...s]
+  if(s.slice(-1) == "!") s.pop();
+  else return s.join('');
+  return remove(s);
+}
+
+// Another Solution
+function remove(s){
+  return s.replace(/!+$/g, '');
+}
+
+// Another Solution
+function remove (s){
+  s = [...s];
+  while(s.slice(-1) == "!") {s.pop();}
+  return s.join('');
+}
+
+// Another Solution
+function remove (s){
+  while(s.slice(-1) === "!")  s = s.slice(0, -1);
+  return s;
+}
+
+console.log(remove("!Hi! Hi!!!"));    // -> !Hi! Hi
