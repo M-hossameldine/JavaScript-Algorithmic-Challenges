8824e6ffb7c7520de168a344df0cb015b5666ef7
Mohamed Hossam <m.hossameldine@gmail.com>
2021-09-29 03:35:09 +0200

    7 Kyu - Negative Connotaion

---
+++ CodeWars/Kyu_7-Negative_Connotation/kata.js
@@ -0,0 +1,18 @@
+/*
+Negative Connotation:
+You will be given a string with sets of characters, (i.e. words), seperated by between one and three spaces (inclusive).
+
+Looking at the first letter of each word (case insensitive-"A" and "a" should be treated the same), you need to determine whether it falls into the positive/first half of the alphabet ("a"-"m") or the negative/second half ("n"-"z").
+
+Return True/true if there are more (or equal) positive words than negative words, False/false otherwise.
+
+  "A big brown fox caught a bad rabbit" => True/true
+  "Xylophones can obtain Xenon." => False/false
+*/
+
+function connotation(str) {
+  let positiveWords = str.match(/\b[a-m]/ig) || []; 
+  let negativeWords = str.match(/\b[n-z]/ig) || []; 
+  return positiveWords.length >= negativeWords.length ;
+}
+
