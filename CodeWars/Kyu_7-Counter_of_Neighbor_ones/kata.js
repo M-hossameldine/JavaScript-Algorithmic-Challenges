9ebaea9034919af9da203762626f3fab255e0957
Mohamed Hossam <m.hossameldine@gmail.com>
2021-09-30 23:20:15 +0200

    7 kyu - Counter of neighbor ones

---
+++ CodeWars/Kyu_7-Counter_of_Neighbor_ones/kata.js
@@ -0,0 +1,13 @@
+/*
+Counter of neighbor ones: 
+Task
+Tranform of input array of zeros and ones to array in which counts number of continuous ones. If there is none, return an empty array
+
+Example
+  [1, 1, 1, 0, 1] -> [3,1]
+  [1, 1, 1, 1, 1] -> [5]
+  [0, 0, 0, 0, 0] -> []
+*/
+function onesCounter(input) {
+  return (input.join('').match(/1+/g) || []).map( str => str.length );
+}
