8e92649b634b6c09b1057664a403095ab78675be
Mohamed Hossam <m.hossameldine@gmail.com>
2021-10-20 01:08:30 +0200

    7 Kyu - Sum of Cubes

---
+++ CodeWars/Kyu_7-Sum_of_Cubes/kata.js
@@ -0,0 +1,22 @@
+function sumCubes(n){
+  let sum = 0;
+  let i = 1;
+  while(i <= n) {
+    sum += Math.pow(i, 3);
+    i++;
+  }
+  return sum;
+}
+
+// Another Solution
+function sumCubes(n){
+  if(n == 1) return n;
+  return Math.pow(n, 3) + sumCubes(n - 1)
+}
+
+// Solution by other Code Warriors
+// https://mathschallenge.net/library/number/sum_of_cubes
+
+function sumCubes(n){
+  return (n*(n+1)/2)**3;
+}
